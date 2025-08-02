cffae920c1491a701dcb8effc0960206e68cff1e
Himanshu-Kumar-Sah <himanshusah4114@gmail.com>
2025-08-02 22:06:05 +0530

    Add sample config file (config_sample.py)

---
+++ config_sample.py
@@ -0,0 +1,13 @@
+DB_HOST = "localhost"
+DB_USER = "your_mysql_user"
+DB_PASS = "your_mysql_password"
+DB_NAME = "user_database"
+
+Admin1_username = "admin_username"
+Admin1_password = "admin_password"  # Will be hashed internally
+
+MAIL_USERNAME = "your-email@example.com"
+MAIL_PASSWORD = "your-app-password"
+MAIL_SERVER = "smtp.gmail.com"
+MAIL_PORT = 587
+MAIL_USE_TLS = True
