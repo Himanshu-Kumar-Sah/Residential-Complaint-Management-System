1489271306a095c1f5de63ad6b6faa71f439a03e
Himanshu-Kumar-Sah <148904866+Himanshu-Kumar-Sah@users.noreply.github.com>
2025-12-30 00:54:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1133,5 +1133,7 @@ def reset_password():
 
 
 
+# if __name__ == "__main__":
+    # app.run(host="0.0.0.0", port=5000, debug=True)
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run()
