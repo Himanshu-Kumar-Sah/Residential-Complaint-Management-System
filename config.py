8fd9749972d697effc93f32780c7b592f0f248c4
Himanshu-Kumar-Sah <148904866+Himanshu-Kumar-Sah@users.noreply.github.com>
2025-12-30 16:08:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ DB_HOST = os.getenv("DB_HOST")
 DB_USER = os.getenv("DB_USER")
 DB_PASS = os.getenv("DB_PASS")
 DB_NAME = os.getenv("DB_NAME")
+DB_PORT = int(os.getenv("DB_PORT", 3306)) 
 
 MAIL_SERVER = os.getenv("MAIL_SERVER")
 MAIL_PORT = int(os.getenv("MAIL_PORT", 587))
@@ -11,6 +12,5 @@ MAIL_USE_TLS = True
 MAIL_USERNAME = os.getenv("MAIL_USERNAME")
 MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
 
-# Optional (admin auto-create)
-Admin1_username = os.getenv("ADMIN_USERNAME", "admin")
-Admin1_password = os.getenv("ADMIN_PASSWORD", "admin123")
+ADMIN_USERNAME = os.getenv("ADMIN_USERNAME")
+ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD")
